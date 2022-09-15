2c3d56c72d54929d3e91a527530e3ac23e5150ac
JuanTamadski <76487328+JuanTamadski@users.noreply.github.com>
2022-09-15 22:40:28 +0800

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aicp_Z01R.mk
+    $(LOCAL_DIR)/Lineage_Z01R.mk
 
 COMMON_LUNCH_CHOICES := \
-    aicp_Z01R-user \
-    aicp_Z01R-userdebug \
-    aicp_Z01R-eng
+    Lineage_Z01R-user \
+    Lineage_Z01R-userdebug \
+    Lineage_Z01R-eng
