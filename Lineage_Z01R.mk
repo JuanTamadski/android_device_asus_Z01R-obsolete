c2f26436f5bd91b9d3bb672649f8016c570c5beb
JuanTamadski <76487328+JuanTamadski@users.noreply.github.com>
2022-09-15 22:53:46 +0800

    Update Lineage_Z01R.mk

--- Lineage_Z01R.mk
+++ Lineage_Z01R.mk
@@ -25,16 +25,54 @@ $(call inherit-product, device/asus/Z01R/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# maintainer flag (one word only/no spacing)
+RICE_MAINTAINER := JuanTamadski
 
-WITH_GAPPS := true
+# chipset flag (one word only/no spacing)
+RICE_CHIPSET := SD845
+
+# gapps build flag, if not defined build type is vanilla
+WITH_GMS := true
+
+# Sushi Bootanimation (only 720/1080p/1440 supported. if not defined, bootanimation is google bootanimation)
+SUSHI_BOOTANIMATION := 1080
+
+# Graphene Camera
+TARGET_BUILD_GRAPHENEOS_CAMERA := true
+
+# disable/enable blur support, default is false
+TARGET_ENABLE_BLUR := true
+
+# UDFPS ICONS/ANIMATIONS
+TARGET_HAS_UDFPS := false
+
+# Quick tap feature
+TARGET_SUPPORTS_QUICK_TAP := true
+
+# Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# Memory properties
+# Tune these properties according to device perfconfigstore.xml(if available)/device capabilities.
+# These props are overridable
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.sys.fw.bg_apps_limit?=48 \
+    persist.sys.fw.use_trim_settings?=true \
+    persist.sys.fw.empty_app_percent?=50 \
+    persist.sys.fw.trim_empty_percent?=100 \
+    persist.sys.fw.trim_cache_percent?=100 \
+    persist.sys.fw.trim_enable_memory?=2147483648 \
+    persist.sys.fw.bservice_age?=120000 \
+    persist.sys.fw.bservice_limit?=6 \
+    persist.sys.fw.bservice_enable?=true 
+
 TARGET_USE_PIXEL_CHARGER := true
-TARGET_BOOT_ANIMATION_RES := 1080
 
 PRODUCT_BRAND := asus
 PRODUCT_DEVICE := Z01R
 PRODUCT_MANUFACTURER := asus
 PRODUCT_MODEL := Zenfone 5Z
-PRODUCT_NAME := aicp_Z01R
+PRODUCT_NAME := lineage_Z01R
 
 PRODUCT_GMS_CLIENTID_BASE := android-asus
 
