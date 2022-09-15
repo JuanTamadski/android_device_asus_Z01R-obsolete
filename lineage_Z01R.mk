9e1cde41995c13a3b002d5ebba96887683e6d2c1
JuanTamadski <76487328+JuanTamadski@users.noreply.github.com>
2022-09-15 14:03:54 +0800

    Update lineage_Z01R.mk

--- lineage_Z01R.mk
+++ lineage_Z01R.mk
@@ -23,13 +23,18 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
 $(call inherit-product, device/asus/Z01R/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aicp/config/common_full_phone.mk)
+
+
+WITH_GAPPS := true
+TARGET_USE_PIXEL_CHARGER := true
+TARGET_BOOT_ANIMATION_RES := 1080
 
 PRODUCT_BRAND := asus
 PRODUCT_DEVICE := Z01R
 PRODUCT_MANUFACTURER := asus
 PRODUCT_MODEL := Zenfone 5Z
-PRODUCT_NAME := lineage_Z01R
+PRODUCT_NAME := aicp_Z01R
 
 PRODUCT_GMS_CLIENTID_BASE := android-asus
 
