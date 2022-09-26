3773287f1d942fe2347381aa593185df9fc5b95a
JuanTamadski <76487328+JuanTamadski@users.noreply.github.com>
2022-09-26 22:10:24 +0800

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aicp_Z01R.mk
+    $(LOCAL_DIR)/soark_Z01R.mk
 
 COMMON_LUNCH_CHOICES := \
-    aicp_Z01R-user \
-    aicp_Z01R-userdebug \
-    aicp-eng
+    spark_Z01R-user \
+    spark_Z01R-userdebug \
+    spark-eng \
