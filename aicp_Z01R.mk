fa8f322360d33581ad2672a3a432d374a62fd2bc
JuanTamadski <76487328+JuanTamadski@users.noreply.github.com>
2022-09-26 22:06:51 +0800

    Update and rename spark_Z01R.mk to aicp_Z01R.mk

--- aicp_Z01R.mk
+++ aicp_Z01R.mk
@@ -23,7 +23,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
 $(call inherit-product, device/asus/Z01R/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/spark/config/common_full_phone.mk)
+$(call inherit-product, vendor/aicp/config/common_full_phone.mk)
 
 
 WITH_GAPPS := true
@@ -34,7 +34,7 @@ PRODUCT_BRAND := asus
 PRODUCT_DEVICE := Z01R
 PRODUCT_MANUFACTURER := asus
 PRODUCT_MODEL := Zenfone 5Z
-PRODUCT_NAME := spark_Z01R
+PRODUCT_NAME := aicp_Z01R
 
 PRODUCT_GMS_CLIENTID_BASE := android-asus
 
