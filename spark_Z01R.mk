74799cd31276f435c70fb3054faae3af263bf4d1
JuanTamadski <76487328+JuanTamadski@users.noreply.github.com>
2022-10-19 22:13:38 +0800

    Update spark_Z01R.mk

--- spark_Z01R.mk
+++ spark_Z01R.mk
@@ -23,14 +23,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
 $(call inherit-product, device/asus/Z01R/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/spark/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 
 PRODUCT_BRAND := asus
 PRODUCT_DEVICE := Z01R
 PRODUCT_MANUFACTURER := asus
 PRODUCT_MODEL := Zenfone 5Z
-PRODUCT_NAME := spark_Z01R
+PRODUCT_NAME := aosp_Z01R
 
 PRODUCT_GMS_CLIENTID_BASE := android-asus
 
@@ -44,7 +44,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 BUILD_FINGERPRINT := asus/WW_Z01RD/ASUS_Z01R_1:8.0.0/OPR1.170623.032/WW_80.30.96.221_20181018:user/release-keys
 
-SPARK_BUILD_TYPE := UNOFFICIAL
-TARGET_USES_BLUR := true
-WITH_FOD_ANIMATIONS := false
 TARGET_FACE_UNLOCK_SUPPORTED := true
